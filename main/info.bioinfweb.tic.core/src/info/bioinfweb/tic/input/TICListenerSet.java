c76552bd1c87974a403d3fdb00519fb089c7092a
BenStoever <stoever@bioinfweb.info>
2016-12-08 16:55:57 +0000

    License information updated in core module.

--- main/info.bioinfweb.tic.core/src/info/bioinfweb/tic/input/TICListenerSet.java
+++ main/info.bioinfweb.tic.core/src/info/bioinfweb/tic/input/TICListenerSet.java
@@ -1,19 +1,19 @@
 /*
- * bioinfweb.commons - Shared components of bioinfweb projects made available in a Java library
- * Copyright (C) 2014 - 2015  Ben Stöver
- * <http://commons.bioinfweb.info/Java>
+ * Toolkit independent components (TIC) - A Java library for creating GUI components for Swing and SWT
+ * Copyright (C) 2014-2016  Ben Stöver
+ * <http://bioinfweb.info/TIC>
  * 
  * This file is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package info.bioinfweb.tic.input;
