48eded7ea8033deefcbf53887f886b7b716bbab7
BenStoever <stoever@bioinfweb.info>
2017-09-12 11:46:05 +0000

    Direct scrolling demo extended to show programmatic scrolling.

--- demo/info.bioinfweb.tic.demo.directscrolling/src/info/bioinfweb/tic/demo/directscrolling/SWTApplication.java
+++ demo/info.bioinfweb.tic.demo.directscrolling/src/info/bioinfweb/tic/demo/directscrolling/SWTApplication.java
@@ -19,11 +19,18 @@
 package info.bioinfweb.tic.demo.directscrolling;
 
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
 import info.bioinfweb.tic.SWTComponentFactory;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
 
@@ -78,5 +85,36 @@ public class SWTApplication {
 		
 		// Create SWT-specific component instance and add it to the SWT GUI:
 		SWTComponentFactory.getInstance().getSWTComponent(scrollingComponent, shell, SWT.NONE);
+		
+		// Create main menu to demonstrate programmatic scrolling:
+		Menu menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+		
+		MenuItem mntmScroll = new MenuItem(menu, SWT.CASCADE);
+		mntmScroll.setText("Scroll");
+		
+		Menu menuScroll = new Menu(mntmScroll);
+		mntmScroll.setMenu(menuScroll);
+		
+		MenuItem mntmTopLeft = new MenuItem(menuScroll, SWT.NONE);
+		mntmTopLeft.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				scrollingComponent.setScrollOffset(0, 0);
+			}
+		});
+		mntmTopLeft.setText("Top left");
+		
+		MenuItem mntmCenter = new MenuItem(menuScroll, SWT.NONE);
+		mntmCenter.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Dimension size = scrollingComponent.getSize();
+				Rectangle r = scrollingComponent.getVisibleRectangle();
+				scrollingComponent.setScrollOffset((size.width - r.width) / 2, (size.height - r.height) / 2);
+			}
+		});
+		mntmCenter.setText("Center");
+		
 	}
 }
