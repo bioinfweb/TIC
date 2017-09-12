9b9b725036bc944a2095f49ff81888cdd87dc995
BenStoever <stoever@bioinfweb.info>
2017-09-12 11:47:00 +0000

    Removed unused imports.

--- demo/info.bioinfweb.tic.demo.scrollcontainer/src/info/bioinfweb/tic/demo/scrollcontainer/SwingApplication.java
+++ demo/info.bioinfweb.tic.demo.scrollcontainer/src/info/bioinfweb/tic/demo/scrollcontainer/SwingApplication.java
@@ -22,19 +22,16 @@ package info.bioinfweb.tic.demo.scrollcontainer;
 import info.bioinfweb.tic.SwingComponentFactory;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.UIManager;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 
 
